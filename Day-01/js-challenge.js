f4e1ab851067ff1294cbe528667f31287109ec2c
Tapas Ranjan Patra <tapash0382@gmail.com>
2026-07-11 22:22:20 +0530

    Day 1 completed

---
+++ Day-01/js-challenge.js
@@ -0,0 +1,26 @@
+const numbers = [12, 46, 7, 89, 23, 45, 12, 67, 1, 89];
+
+const max = Math.max(...numbers);
+const min = Math.min(...numbers);
+
+const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+const average = sum / numbers.length;
+
+const uniqueNumbers = [...new Set(numbers)];
+
+const asc = [...uniqueNumbers].sort((a,b) => a - b);
+const desc = [...uniqueNumbers].sort((a, b) => b - a);
+
+const frequency = numbers.reduce((acc, num) => {
+    acc[num] = (acc[num] || 0) + 1;
+    return acc;
+}, {});
+
+console.log("Max: ", max);
+console.log("Min: ", min);
+console.log("Sum: ", sum);
+console.log("Average: ", average);
+console.log("Unique: ", uniqueNumbers);
+console.log("Ascending order: ", asc);
+console.log("Descending Order: ", desc);
+console.log("frequency: ", frequency);
