c4f82ee154430c41e010a1757d6be8f911ad4602
Tapas Ranjan Patra <tapash0382@gmail.com>
2026-07-16 23:44:46 +0530

    Add js-challenge-6.js implementing Merge Sorted Array algorithm using Two Pointers

---
+++ Day-06/js-challenge-6.js
@@ -0,0 +1,23 @@
+// Merge Sorted Array (Two Pointers starting from the end)
+function merge(nums1, m, nums2, n) {
+    let i = m - 1;
+    let j = n - 1;
+    let k = m + n - 1;
+
+    while (j >= 0) {
+        if(i >= 0 && nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+}
+const nums1 = [1, 2, 3, 0, 0, 0];
+const nums2 = [2, 5, 6];
+
+merge(nums1, 3, nums2, 3);
+
+console.log(nums1);  // [1, 2, 2, 3, 5, 6]
