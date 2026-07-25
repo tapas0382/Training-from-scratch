5e73406b7bd59c55c7217855bdaf0a96dd70e328
Tapas Ranjan Patra <tapash0382@gmail.com>
2026-07-25 14:11:10 +0530

    Refactor frequency-2.js to improve character frequency calculation and output

--- Day-08-rough/frequency-2.js
+++ Day-08-rough/frequency-2.js
@@ -7,5 +7,20 @@ for (let char of str) {
     // if (frequency[char]) frequency[char]++;
     // else frequency[char] = 1;
 };
-
 console.log(`frequency of each characters in the string is ${JSON.stringify(frequency)}`);
+
+let maxCount = 0;
+
+// Find the maximum frequency
+for (let char in frequency) {
+    if (frequency[char] > maxCount) {
+        maxCount = frequency[char];
+    }
+};
+
+// Print all characters with that frequency
+for (let char in frequency) {
+    if (frequency[char] === maxCount) {
+        console.log(`${char} -> ${maxCount}`);
+    }
+};
