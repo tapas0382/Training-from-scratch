a5bd632e9c8ff4cc77eacf7f248f1892a470a4b9
Tapas Ranjan Patra <tapash0382@gmail.com>
2026-07-25 13:59:35 +0530

    Add frequency-2.js to calculate character frequency in a string

---
+++ Day-08-rough/frequency-2.js
@@ -0,0 +1,11 @@
+const str = "programming";
+let frequency = {};
+
+for (let char of str) {
+    frequency[char] = (frequency[char] || 0) + 1;
+
+    // if (frequency[char]) frequency[char]++;
+    // else frequency[char] = 1;
+};
+
+console.log(`frequency of each characters in the string is ${frequency}`);
