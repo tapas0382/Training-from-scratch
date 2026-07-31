0675303846b34dba8d33428b7070ef82542b2e15
Tapas Ranjan Patra <tapash0382@gmail.com>
2026-07-31 14:54:29 +0530

    Add first-occurrence.js to implement a function for finding the first occurrence of a target in a sorted array

---
+++ Day-09-rought/first-occurrence.js
@@ -0,0 +1,18 @@
+function firstOccurrence (arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+    let answer = -1
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            answer = mid;
+            right = mid - 1;
+        }
+        else if (target < arr[mid]) right = mid - 1;
+        else if (target > arr[mid]) left = mid + 1;
+    };
+    return answer;
+};
+console.log(firstOccurrence([1,2,2,2,3,4], 3));
