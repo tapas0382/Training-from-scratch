285adbc4d080f1cae3750bdcc9c0bcaa3c0edadf
Tapas Ranjan Patra <tapash0382@gmail.com>
2026-09-22 13:54:44 +0530

    Add Dutch National Flag algorithm implementation

---
+++ Day-08/dutch-national-flag-algo.py
@@ -0,0 +1,24 @@
+def sort_risk_levels(arr):
+    low = 0
+    mid = 0
+    high = len(arr) - 1
+
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+
+    return arr
+
+N = 7
+arr = [0, 1, 2, 0, 1, 2, 1]
+
+print("Input: ", arr)
+result = sort_risk_levels(arr)
+print("Output: ", result)
