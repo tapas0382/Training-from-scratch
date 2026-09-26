d0095036e7d6150ffde6789e6f82149394ad301e
Tapas Ranjan Patra <tapash0382@gmail.com>
2026-09-26 21:32:21 +0530

    Add initial implementation of task-1.py with variable types and basic operations

---
+++ Day-09/task-1.py
@@ -0,0 +1,33 @@
+age = 25
+print(type(age))
+
+percentage = 82.05
+print(type(percentage))
+
+name = 'Tapas'
+print(type(name))
+
+eligible = True
+print(type(eligible))
+
+# i dont know about none
+
+sum = 2 + 3
+sub = 5 - 2
+mul = 6 * 5
+div = 8 / 4
+
+# i dont know use of //
+
+mod = 98 % 10  #answer will be 8
+
+# i dont know use of **
+
+# i know use of comparison operators, no need to write here
+
+if eligible and name:
+    print(name)         # it will print the name cause both are true
+
+# i dont know how to use not, give me example
+
+# i see there are much similarities between python and javascript but these are different, there syntax are very different, python have very smaller and simpler syntaxes, types should be define in javascript
